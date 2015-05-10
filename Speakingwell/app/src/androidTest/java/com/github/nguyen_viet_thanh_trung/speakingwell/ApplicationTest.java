44cc1b49598f1f2c0767dbb7b72429acb75f474b
trung <nguyen.v.t.trung@gmail.com>
2015-05-10 13:45:14 +0200

    update GUI and permisions

---
+++ Speakingwell/app/src/androidTest/java/com/github/nguyen_viet_thanh_trung/speakingwell/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.github.nguyen_viet_thanh_trung.speakingwell;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
